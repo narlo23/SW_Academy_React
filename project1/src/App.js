bdd12ac0a5b0fbddc744f942748f8f2001a3b483
narlo <aks152312@gmail.com>
2023-01-06 16:15:22 +0900

    style

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,33 +1,11 @@
-import { useState, useEffect } from "react";
-import Counter from "./Components/Counter";
+import Box from "./Components/Box";
+import { css } from "@emotion/react";
+
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    console.log(`Clicked ${count} times.`);
-  }, [count]);
-
-  useEffect(() => {
-    console.log("Component Loaded");
-    const handleScroll = () => {
-      console.log(window.scrollY);
-    };
-    document.addEventListener("scroll", handleScroll);
-    //컴포넌트가 제거될 때 실행
-    return () => document.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <div>
-      <div>You clicked {count} times</div>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        +
-      </button>
-      <div style={{ height: 10000 }}></div>
+      <div css={{ width: 200, height: 100, backgroundColor: "black" }} />
+      <Box />
     </div>
   );
 }
