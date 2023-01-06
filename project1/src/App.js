8e0f3908159a8683f36d3b8bd3968f3e4bcfb765
narlo <aks152312@gmail.com>
2023-01-06 16:31:04 +0900

    useMemo 사용해보기

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,33 +1,12 @@
-import { useState, useEffect } from "react";
-import Counter from "./Components/Counter";
+import { useState } from "react";
+import ShowSum from "./Components/ShowSum";
+
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    console.log(`Clicked ${count} times.`);
-  }, [count]);
-
-  useEffect(() => {
-    console.log("Component Loaded");
-    const handleScroll = () => {
-      console.log(window.scrollY);
-    };
-    document.addEventListener("scroll", handleScroll);
-    //컴포넌트가 제거될 때 실행
-    return () => document.removeEventListener("scroll", handleScroll);
-  }, []);
-
+  const [label, setLabel] = useState("Result");
   return (
     <div>
-      <div>You clicked {count} times</div>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        +
-      </button>
-      <div style={{ height: 10000 }}></div>
+      <button onClick={() => setLabel(label + ":")}>Change Label</button>
+      <ShowSum label={label} n={10000000000} />
     </div>
   );
 }
