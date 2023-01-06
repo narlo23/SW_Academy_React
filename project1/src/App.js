60f07522add4494a2b5dc2d76a2fc5941dc1de6b
narlo <aks152312@gmail.com>
2023-01-06 15:00:09 +0900

    Counter component 구현

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,43 +1,35 @@
 import { useState } from "react";
-import Board from "./Components/Board";
+import Counter from "./Components/Counter";
 function App() {
-  const [visible, setVisible] = useState(false);
-
-  const articles = [
-    {
-      id: 1,
-      title: "안녕",
-      author: "narlo",
-    },
-    {
-      id: 2,
-      title: "ㅎㅇ",
-      author: "narlo",
-    },
-    {
-      id: 3,
-      title: "hi",
-      author: "narlo",
-    },
-    {
-      id: 4,
-      title: "hello",
-      author: "narlo",
-    },
-  ];
+  const [totalCount, setTotalCount] = useState(0);
   return (
     <div>
-      <button onClick={() => setVisible(!visible)}>Toggle</button>
-      {visible ? (
-        <h1>논리곱 연산자를 통해 쉽게 JSX 렌더링 여부를 결정할 수 있습니다.</h1>
-      ) : null}
-      {visible ? (
-        <Board articles={articles} />
-      ) : (
-        <p>게시판을 보려면 Toggle 버튼을 클릭해주세요.</p>
-      )}
+      TotalCount: {totalCount}
+      <Counter
+        onIncrease={(count) => {
+          setTotalCount(totalCount + 1);
+        }}
+        onDecrease={(count) => {
+          setTotalCount(totalCount - 1);
+        }}
+      />
+      <Counter
+        onIncrease={(count) => {
+          setTotalCount(totalCount + 1);
+        }}
+        onDecrease={(count) => {
+          setTotalCount(totalCount - 1);
+        }}
+      />
+      <Counter
+        onIncrease={(count) => {
+          setTotalCount(totalCount + 1);
+        }}
+        onDecrease={(count) => {
+          setTotalCount(totalCount - 1);
+        }}
+      />
     </div>
   );
 }
-
 export default App;
---
+++ project1/src/Components/Counter/index.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+function Counter({ onIncrease, onDecrease }) {
+  const [count, setCount] = useState(0);
+
+  const handleIncrease = () => {
+    setCount(count + 1);
+    if (onIncrease) {
+      onIncrease(count + 1);
+    }
+  };
+
+  const handleDecrease = () => {
+    setCount(count - 1);
+    if (onDecrease) {
+      onDecrease(count - 1);
+    }
+  };
+  return (
+    <div>
+      <span style={{ fontSize: 40 }}> {count}</span>
+      <br />
+      <button onClick={handleIncrease}>+</button>
+      <button onClick={handleDecrease}>-</button>
+    </div>
+  );
+}
+
+Counter.propTypes = {
+  onIncrease: PropTypes.func,
+  onDecrease: PropTypes.func,
+};
+
+export default Counter;
