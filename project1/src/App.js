1de99905029777c71b6c5c342eb7fa8881eb8b34
narlo <aks152312@gmail.com>
2023-01-06 15:42:33 +0900

    페이지네이션

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,33 +1,25 @@
-import { useState, useEffect } from "react";
-import Counter from "./Components/Counter";
+import { useState } from "react";
+import Board from "./Components/Board";
+import Pagination from "./Components/Pagination";
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [page, setPage] = useState(0);
+  const articles = new Array(100).fill().map((_, i) => ({
+    id: i,
+    title: `${i}번 게시물`,
+  }));
 
-  useEffect(() => {
-    console.log(`Clicked ${count} times.`);
-  }, [count]);
-
-  useEffect(() => {
-    console.log("Component Loaded");
-    const handleScroll = () => {
-      console.log(window.scrollY);
-    };
-    document.addEventListener("scroll", handleScroll);
-    //컴포넌트가 제거될 때 실행
-    return () => document.removeEventListener("scroll", handleScroll);
-  }, []);
-
+  const limit = 6;
+  const offset = page * limit;
   return (
     <div>
-      <div>You clicked {count} times</div>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        +
-      </button>
-      <div style={{ height: 10000 }}></div>
+      <Pagination
+        defaultPage={0}
+        limit={limit}
+        total={articles.length}
+        onChange={setPage}
+      />
+      <Board articles={articles.slice(offset, offset + limit)} />
     </div>
   );
 }
--- project1/src/Components/Board.js
+++ project1/src/Components/Board.js
@@ -1,20 +1,18 @@
 import PropTypes from "prop-types";
 const Board = ({ articles }) => {
   return (
-    <div>
-      <h1>Board</h1>
-      <ul>
-        {articles.map((article) => (
-          <li key={article.id}>
-            {article.id} | {article.title} | {article.author}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul>
+      {articles.map((article) => (
+        <li key={article.id}>
+          {article.id} | {article.title}
+        </li>
+      ))}
+    </ul>
   );
 };
 
 Board.propTypes = {
   articles: PropTypes.array,
 };
+
 export default Board;
