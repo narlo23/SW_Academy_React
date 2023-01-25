740c9c8cb133b20c62e46221efa0dc4a925e16b5
narlo <aks152312@gmail.com>
2023-01-25 13:40:22 +0900

    storybook

---
+++ upload/src/components/Avatar/index.js
@@ -0,0 +1,15 @@
+const Avatar = ({
+  lazy,
+  threshold,
+  src,
+  size = 70,
+  shape = "circle",
+  placeholder,
+  alt,
+  mode = "dover",
+  ...props
+}) => {
+  return <div />;
+};
+
+export default Avatar;
