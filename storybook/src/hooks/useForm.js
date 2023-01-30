c3f3332354451091c1a317748b8e52b055590599
narlo <aks152312@gmail.com>
2023-01-30 17:38:45 +0900

    storybook

--- storybook/src/hooks/useForm.js
+++ storybook/src/hooks/useForm.js
@@ -13,9 +13,9 @@ const useForm = ({ initialValues, onSubmit, validate }) => {
   const handleSubmit = async (e) => {
     setIsLoading(true);
     e.preventDefault();
-    const newErrors = validate(values);
+    const newErrors = validate ? validate(values) : {};
     if (Object.keys(newErrors).length === 0) {
-      await onSubmit();
+      await onSubmit(values);
     }
     setErrors(newErrors);
     setIsLoading(false);
